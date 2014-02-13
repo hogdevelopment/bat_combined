cfafbb39885818e8aa72847f0dae0b7360029680
Chiosa Gabi <stefanvladut@infoprint-s3.idgroup.local>
2014-02-13 16:20:08 +0200

    - Added Attributed Key Collection View Controller class as a starter point.

---
+++ BespokeLocationV2/RMNAttributesKeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  RMNAttributesKeyViewController.h
+//  BespokeLocationV2
+//
+//  Created by Chiosa Gabi on 13/02/14.
+//  Copyright (c) 2014 Hogarth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RMNAttributesKeyViewController : UICollectionViewController
+
+@end
---
+++ BespokeLocationV2/RMNAttributesKeyViewController.m
@@ -0,0 +1,38 @@
+//
+//  RMNAttributesKeyViewController.m
+//  BespokeLocationV2
+//
+//  Created by Chiosa Gabi on 13/02/14.
+//  Copyright (c) 2014 Hogarth. All rights reserved.
+//
+
+#import "RMNAttributesKeyViewController.h"
+
+@interface RMNAttributesKeyViewController ()
+
+@end
+
+@implementation RMNAttributesKeyViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
