719b6021f6abd2645560716139d7afc454ee11d8
Chiosa Gabi <stefanvladut@idpdpd010.idgroup.local>
2014-02-14 16:49:05 +0200

    - Re-added Core Data to project.

--- BespokeLocationV2/RMNSettings.h
+++ BespokeLocationV2/RMNSettings.h
@@ -9,7 +9,16 @@
 #ifndef BespokeLocationV2_RMNSettings_h
 #define BespokeLocationV2_RMNSettings_h
 
+// test if the curred device is iphone 5, 5s
 #define IS_IPHONE_5 ( fabs( ( double )[ [ UIScreen mainScreen ] bounds ].size.height - ( double )568 ) < DBL_EPSILON )
 
+// test if the curred device is a iphone device
+#define IS_IPHONE ( [ [ [ UIDevice currentDevice ] model ] isEqualToString: @"iPhone" ] )
+
+// test if the curred device is a ipod device
+#define IS_IPOD   ( [ [ [ UIDevice currentDevice ] model ] isEqualToString: @"iPod touch" ] )
+
+// test if the curred device is orientated to a landscape format
+#define IS_WIDESCREEN ( fabs( ( double )[ [ UIScreen mainScreen ] bounds ].size.height - ( double )568 ) < DBL_EPSILON )
 
 #endif
