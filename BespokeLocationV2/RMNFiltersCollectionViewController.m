fa74c560046f65492fd5e3b48cbd26a520b1c1a3
Chiosa Gabi <stefanvladut@idpdpd010.idgroup.local>
2014-03-10 18:51:56 +0200

    - Added funcitonality to the clear filters/ find button from the button of the Filters Side Menu Controller. Now it only displays the selected filters/attributes in debug.

--- BespokeLocationV2/RMNFiltersCollectionViewController.m
+++ BespokeLocationV2/RMNFiltersCollectionViewController.m
@@ -120,10 +120,9 @@
     UIImage *findImage = [RMNCustomNavButton customNavButton:RMNCustomNavButtonArrowless withTitle:@"Find"];
     [self.findVenuesButton setImage:findImage forState:UIControlStateNormal];
     
-    
-    
-    
 
+    
+    
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -296,7 +295,7 @@
         
         [[sourceArray objectAtIndex:indexPath.row]setValue:@"selected" forKey:@"state"];
         
-        
+        NSLog(@"baga filtru");
         [selectedFilters addObject:[sourceArray objectAtIndex:indexPath.row]];
         
     }
@@ -340,7 +339,7 @@
                 break;
         }
         
-        
+        NSLog(@"scoate filtru");
         [[sourceArray objectAtIndex:indexPath.row] setValue:@"deselected" forKey:@"state"];
         [selectedFilters removeObject:[sourceArray objectAtIndex:indexPath.row]];
 
@@ -366,16 +365,36 @@
 
 - (IBAction)clearFilters:(UIButton *)sender
 {
-    NSLog(@"Clear filters");
-    // reset the state for each filter
-    for (NSMutableDictionary *info  in selectedFilters)
-    {
-        [info setValue:@"deselected" forKey:@"state"];
-        [selectedFilters removeObject:info];
-    }
+    NSLog(@"Clear filters %d",[selectedFilters count]);
+
     
-    // reload the collection view
-    [self.collectionView reloadData];
+    
+    // find array of pins which souldn't be on the map but are and remove references
+    dispatch_async(kBgQueue, ^{
+        
+
+        // reset the state for each filter
+        for (int i = 0; i<[selectedFilters count];i++)
+        {
+            NSMutableDictionary *info = [selectedFilters objectAtIndex:i];
+            [info setValue:@"deselected" forKey:@"state"];
+            NSLog(@"SCOATE %@",[info valueForKey:@"text"]);
+           
+        }
+        
+        [selectedFilters removeAllObjects];
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            
+            NSLog(@"ramane cu %d elemente ",[selectedFilters count]);
+            // reload the collection view
+            [self.collectionView reloadData];
+            
+            
+        });
+    });
+    
+
     
 }
 
